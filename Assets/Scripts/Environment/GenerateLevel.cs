045670b813927753a6ac68151fc1b6f313d24450
MertArar <mertarar.ma@gmail.com>
2024-01-29 04:43:34 +0300

    Level generator added

---
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject[] section;
+    public int zPos = 50;
+    public bool creatingSection = false;
+    public int secNum;
+
+    private void Update()
+    {
+        if (creatingSection == false)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }
+    }
+    IEnumerator GenerateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        zPos += 50;
+        yield return new WaitForSeconds(2);
+        creatingSection = false;
+    }
+}
