9a2dd19007aad36cc6551a0ada02b18c758ddae0
MertArar <mertarar.ma@gmail.com>
2024-01-30 05:46:55 +0300

    UI and background music added

---
+++ Assets/Scripts/Collectables/LevelDistance.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDistance : MonoBehaviour
+{
+    public GameObject disDisplay;
+    public int disRun;
+    public bool addingDis = false;
+    public float disDelay = 0.5f;
+
+    void Update()
+    {
+        if (addingDis == false)
+        {
+            addingDis = true;
+            StartCoroutine(AddingDis());
+        }    
+    }
+
+    IEnumerator AddingDis()
+    {
+        disRun += 1;
+        disDisplay.GetComponent<Text>().text = "" + disRun;
+        yield return new WaitForSeconds(disDelay);
+        addingDis = false;
+    }
+}
